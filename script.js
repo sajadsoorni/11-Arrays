64a76077582bc65b2ec06136645f22a98c073a59
Sajad <sajadsoorni@gmail.com>
2025-04-21 21:12:21 +0100

    chore(loop-methods): compare for-of and forEach for array iteration
    - Used `for...of` with `movements.entries()` to log deposit/withdrawal messages with index
    - Showcased `forEach()` callback parameters `(mov, i, arr)` for the same task
    - Noted that `break` and `continue` cannot be used inside `forEach`, unlike `for...of`

--- script.js
+++ script.js
@@ -71,8 +71,6 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
 /*
 let arr = ['a', 'b', 'c', 'd', 'e'];
@@ -105,7 +103,7 @@ console.log(letters);
 
 console.log(letters.join(' - '));
 */
-
+/*
 const arr = [23, 11, 64];
 console.log(arr[0]);
 console.log(arr.at(0));
@@ -117,3 +115,29 @@ console.log(arr.at(-1));
 
 console.log('sajad'.at(0));
 console.log('sajad'.at(-1));
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited  ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---FOREACH-----------');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
